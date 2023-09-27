f4b751a6c80a094926afed096028892df0283822
Havryliuk Nina <ninagavry07@gmail.com>
2023-09-27 19:26:13 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import { Button, Paper, Stack, Typography } from "@mui/material";
+import { useState } from "react";
+import FeedbackOptions from "./components/FeedbackOptions";
+import Section from "./components/Section";
+import Statistics from "./components/Statistics";
+import Notification from "./components/Notification";
+
+function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  function incrementGood() {
+    setGood(good + 1);
+  }
+  function incrementNeutral() {
+    setNeutral(neutral + 1);
+  }
+  function incrementBad() {
+    setBad((prevBad) => prevBad + 1);
+  }
+  function countTotalFeedback() {
+    const sum = good + neutral + bad; // Обраховуємо суму всіх значень good, neutral, bad
+    return sum;
+  }
+  function countPositiveFeedbackPercentage() {
+    const sum = countTotalFeedback();
+    const percentage = Math.round((good / sum) * 100);
+    return isNaN(percentage) ? 0 : percentage;
+  }
+
+  return (
+    <Paper
+      sx={{
+        padding: "12px",
+      }}
+    >
+      <Stack direction="column" gap="18px">
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            onGoodClick={incrementGood}
+            onNeutralClick={incrementNeutral}
+            onBadClick={incrementBad}
+          />
+        </Section>
+
+        <Section title="Statistics">
+          {countTotalFeedback() === 0 ? (
+            <Notification message="No feedback given" />
+          ) : (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={countTotalFeedback()}
+              positivePercentage={countPositiveFeedbackPercentage()}
+            />
+          )}
+        </Section>
+      </Stack>
+    </Paper>
+  );
+}
+
+export default App;
